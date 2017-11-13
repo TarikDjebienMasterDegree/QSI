2943ddc259e2100f36a83e13ff81b04b1a2e30c9
DJEBIEN Tarik <tarik.djebien@gmail.com>
2017-11-13 02:45:01 +0100

    IS QA

---
+++ CV de Djebien Tarik.mm
@@ -0,0 +1,245 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1349089519944" ID="ID_691677305" MODIFIED="1349111917209">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      MON CV
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1349089843011" HGAP="53" ID="ID_1978521241" MODIFIED="1349111925722" POSITION="right" TEXT="Formation" VSHIFT="-142">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1349089877026" ID="ID_1989346768" MODIFIED="1349111932960" TEXT="Bac S - Lyc&#xe9;e Faidherbe">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1349090079058" ID="ID_4884016" MODIFIED="1349111908825" TEXT="2003-2006">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1349090096122" ID="ID_715369875" MODIFIED="1349111932960" TEXT="PEIP MPSI - Polytech&apos;Lille">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1349090155898" ID="ID_355264169" MODIFIED="1349111908827" TEXT="2006-2008">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1349090187562" ID="ID_563524260" MODIFIED="1349111932960" TEXT="Ecole Sup&#xe9;rieure D&apos;Informatique - SUPINFO Lille">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1349090265203" ID="ID_1674599257" MODIFIED="1349111908829" TEXT="2008-2009">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1349090280313" ID="ID_1632980337" MODIFIED="1349111932961" TEXT="Licence Informatique - Universit&#xe9; Lille1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1349090335794" ID="ID_388354123" MODIFIED="1349111908829" TEXT="2009-2011">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1349090357762" ID="ID_195203040" MODIFIED="1349111932961" TEXT="Master MIAGE - Universit&#xe9; Lille1">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1349090451690" ID="ID_1760991011" MODIFIED="1349111908830" TEXT="2012-2013">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1349090501202" ID="ID_1452146740" MODIFIED="1349111935034" TEXT="Parcours">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1349090538306" ID="ID_616525859" MODIFIED="1349111908831" TEXT="Ing&#xe9;nierie de projets informatiques-nouvelles technologies."/>
+<node COLOR="#111111" CREATED="1349090543986" ID="ID_585451872" MODIFIED="1349111908831" TEXT="Cursus Alternance M1/M2"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1349089975962" HGAP="117" ID="ID_1193380666" MODIFIED="1349111925725" POSITION="left" TEXT="Etat civil" VSHIFT="-123">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1349109509331" ID="ID_1012597342" MODIFIED="1349111908833" TEXT="DJEBIEN TARIK">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1349109661978" ID="ID_847887543" MODIFIED="1349111908833" TEXT="24 ans">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1349109584882" ID="ID_1191383375" MODIFIED="1349111908834" TEXT="47 Rue Louis Spriet 59000 LILLE">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1349109603378" ID="ID_538410283" MODIFIED="1349111908834" TEXT="0602084986">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1349109622058" ID="ID_1586538267" MODIFIED="1349111908835" TEXT="tarik.djebien@gmail.com">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1349109687802" ID="ID_713454524" MODIFIED="1349111908835" TEXT="C&#xe9;libataire">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1349109706130" ID="ID_1554846631" MODIFIED="1349111908836" TEXT="Titulaire du permis B">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1349109875938" ID="ID_1212840440" MODIFIED="1349111925726" POSITION="right" TEXT="Exp&#xe9;riences professionnelles ">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1349109953906" ID="ID_1353177728" MODIFIED="1349111932962" TEXT="Stagiaire - Th&#xe9;matique : d&#xe9;couverte de l&apos;entreprise - juillet 2007">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1349110045730" ID="ID_1569322768" MODIFIED="1349111908837" TEXT="Centre commercial - G&#xe9;ant Casino Roubaix">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1349110077259" ID="ID_94547594" MODIFIED="1349111935034" TEXT="MISSION:  Accompagnement du responsable de maintenance">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1349110097818" ID="ID_1352749585" MODIFIED="1349111936106" TEXT=" entretien des machines et des syst&#xe8;mes">
+<node COLOR="#111111" CREATED="1349110125898" ID="ID_1047544906" MODIFIED="1349111908840" TEXT="&#xe9;lectriques"/>
+<node COLOR="#111111" CREATED="1349110141346" ID="ID_852251508" MODIFIED="1349111908840" TEXT="&#xe9;lectronique"/>
+<node COLOR="#111111" CREATED="1349110158819" ID="ID_1688964118" MODIFIED="1349111908840" TEXT="Informatique"/>
+</node>
+<node COLOR="#111111" CREATED="1349110165834" ID="ID_1891203948" MODIFIED="1349111908840" TEXT="maintenance pr&#xe9;ventive et curative"/>
+</node>
+<node COLOR="#990000" CREATED="1349110183978" ID="ID_161334018" MODIFIED="1349111935035" TEXT="COMP&#xc9;TENCES ACQUISES:">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1349110191050" ID="ID_1213920908" MODIFIED="1349111908841" TEXT="Capacit&#xe9;s d&#x2019;adaptation et d&#x2019;int&#xe9;gration au sein d&#x2019;une &#xe9;quipe."/>
+<node COLOR="#111111" CREATED="1349110201642" ID="ID_1965695629" MODIFIED="1349111908841" TEXT="R&#xe9;activit&#xe9;."/>
+<node COLOR="#111111" CREATED="1349110211690" ID="ID_20331190" MODIFIED="1349111908841" TEXT="Connaissance de l&apos;entreprise."/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1349110269682" ID="ID_1203521609" MODIFIED="1349111932965" TEXT="Stagiaire au sein de l&apos;&#xe9;quipe architecture applicative - avril 2011 - juin 2011">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1349110313146" ID="ID_673406779" MODIFIED="1349111908842" TEXT="Cofidis (Parc scientifique de la haute borne - Villeneuve d&apos;Ascq )">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1349110328234" ID="ID_193584665" MODIFIED="1349111935036" TEXT="MISSION : D&#xe9;veloppement d&apos;une application de cartographie dynamique des environnements du middleware de COFIDIS.">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1349110383658" ID="ID_1896628159" MODIFIED="1349111908849" TEXT="interface graphique en J2SE 1.6 "/>
+<node COLOR="#111111" CREATED="1349110413002" ID="ID_1155980319" MODIFIED="1349111908849" TEXT="topologie et donn&#xe9;es du middleware"/>
+</node>
+<node COLOR="#990000" CREATED="1349110459930" ID="ID_1762812390" MODIFIED="1349111935042" TEXT="COMPETENCES ACQUISES : ">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1349110475362" ID="ID_312499696" MODIFIED="1349111908850" TEXT="- Capacit&#xe9;s d&apos;auto-formation, d&apos;autonomie, d&apos;&#xe9;tude, de conception et de gestion d&apos;un projet."/>
+<node COLOR="#111111" CREATED="1349110488802" ID="ID_13722913" MODIFIED="1349111908850" TEXT="- Acquisition d&apos;un savoir faire en d&#xe9;veloppement d&apos;IHM."/>
+<node COLOR="#111111" CREATED="1349110501330" ID="ID_451114100" MODIFIED="1349111908850" TEXT="- Acquisition d&apos;un savoir &#xea;tre au sein d&apos;une &#xe9;quipe pour mener &#xe0; bien la conduite et la gestion d&apos;un projet."/>
+</node>
+<node COLOR="#990000" CREATED="1349110512978" ID="ID_879378811" MODIFIED="1349111935044" TEXT="COMPETENCES TECHNIQUES MISE EN PRATIQUE :">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1349110522858" ID="ID_1060356456" MODIFIED="1349111908853" TEXT="- J2SE : Java 1.6, API SWING"/>
+<node COLOR="#111111" CREATED="1349110533170" ID="ID_971619699" MODIFIED="1349111908853" TEXT="- J2SE : API SAX/DOM"/>
+<node COLOR="#111111" CREATED="1349110547818" ID="ID_1637636058" MODIFIED="1349111908853" TEXT="- Framework JGraphX"/>
+<node COLOR="#111111" CREATED="1349110556826" ID="ID_1234159103" MODIFIED="1349111908853" TEXT="- XML : DTD, XML-Schema, XPATH 2.0"/>
+<node COLOR="#111111" CREATED="1349110567626" ID="ID_1041670773" MODIFIED="1349111908853" TEXT="- Python : procedural et Orient&#xe9; Objet et Jython"/>
+<node COLOR="#111111" CREATED="1349110579082" ID="ID_1935695912" MODIFIED="1349111908854" TEXT="- Weblogic : WLST (WebLogic Scripting Tools)"/>
+</node>
+<node COLOR="#990000" CREATED="1349110591138" ID="ID_1531176486" MODIFIED="1349111935045" TEXT=" COMPETENCES THEORIQUES MISE EN PRATIQUE :">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1349110597514" ID="ID_1041556088" MODIFIED="1349111908854" TEXT="- Programmation orient&#xe9;e objet et design pattern"/>
+<node COLOR="#111111" CREATED="1349110615690" ID="ID_949241147" MODIFIED="1349111908854" TEXT="- Theorie des graphes et recherche op&#xe9;rationnelle"/>
+<node COLOR="#111111" CREATED="1349110625627" ID="ID_1162624451" MODIFIED="1349111908854" TEXT="- Gestion et conduite de projet"/>
+<node COLOR="#111111" CREATED="1349110635971" ID="ID_1413094786" MODIFIED="1349111908854" TEXT="- Unified Modeling Langage 2"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1349110740298" ID="ID_353052042" MODIFIED="1349111932967" TEXT="Ing&#xe9;nieur d&apos;&#xe9;tude - SSII Ubik Ingenierie - Roubaix">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1349110801242" ID="ID_719420395" MODIFIED="1349111935047" TEXT="Septembre 2011 - Janvier 2012">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1349110832922" ID="ID_923928259" MODIFIED="1349111908855" TEXT="MISSION : Sous la supervision de Mr. MOUAWAD, j&apos;ai particip&#xe9; &#xe0; l&apos;&#xe9;volution et le support de l&apos;application JEE de gestion interne de comptes rendus d&apos;activit&#xe9;s"/>
+<node COLOR="#111111" CREATED="1349110919866" ID="ID_357171343" MODIFIED="1349111936107" TEXT="COMPETENCES ACQUISES : ">
+<node COLOR="#111111" CREATED="1349110924170" ID="ID_581476855" MODIFIED="1349111908858" TEXT="- Capacit&#xe9;s d&apos;auto-formation, d&apos;autonomie, d&apos;adaptation."/>
+<node COLOR="#111111" CREATED="1349110934882" ID="ID_1806411057" MODIFIED="1349111908858" TEXT="- Acquisition d&apos;un savoir faire en d&#xe9;veloppement JEE/Web."/>
+</node>
+<node COLOR="#111111" CREATED="1349110945674" ID="ID_1480893574" MODIFIED="1349111936109" TEXT="COMPETENCES TECHNIQUES MISE EN PRATIQUE :">
+<node COLOR="#111111" CREATED="1349110958594" ID="ID_1296890191" MODIFIED="1349111908858" TEXT="- JEE : API JSP/Servlet"/>
+<node COLOR="#111111" CREATED="1349110968298" ID="ID_601226904" MODIFIED="1349111908858" TEXT="- J2SE : API JDBC"/>
+<node COLOR="#111111" CREATED="1349110977522" ID="ID_1698821263" MODIFIED="1349111908859" TEXT="- Framework Spring (IOC, JDBC, AOP, MVC, Security)"/>
+<node COLOR="#111111" CREATED="1349110996058" ID="ID_930584581" MODIFIED="1349111908859" TEXT="- Framework JQuery"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1349111020609" ID="ID_1299623722" MODIFIED="1349111935050" TEXT="D&#xe9;cembre 2011 - Mars 2012">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1349111054626" ID="ID_1634769243" MODIFIED="1349111908859" TEXT="MISSION : j&apos;ai particip&#xe9; &#xe0; la cr&#xe9;ation et &#xe0; l&apos;&#xe9;volution d&apos;un POC pour une application mobile &quot;Sale Force Mobility&quot; pour le client Lyreco, jusqu&apos;&#xe0; sa livraison. Application permettant &#xe0; la force de vente de g&#xe9;rer son portefeuille clients / prospects et de g&#xe9;n&#xe9;rer des offres de prix."/>
+<node COLOR="#111111" CREATED="1349111135805" ID="ID_142339818" MODIFIED="1349111936110" TEXT="COMPETENCES ACQUISES : ">
+<node COLOR="#111111" CREATED="1349111140258" ID="ID_577423875" MODIFIED="1349111908862" TEXT="- Recherche de nouvelles solutions techniques en r&#xe9;ponse des demandes utilisateurs."/>
+<node COLOR="#111111" CREATED="1349111161970" ID="ID_228016298" MODIFIED="1349111908862" TEXT="- Capacit&#xe9;s d&apos;adaptation, de r&#xe9;activit&#xe9; et de gestion de projet."/>
+<node COLOR="#111111" CREATED="1349111166195" ID="ID_22653490" MODIFIED="1349111936597" TEXT="">
+<node COLOR="#111111" CREATED="1349111174215" ID="ID_613445238" MODIFIED="1349111908863" TEXT="- Acquisition d&apos;un savoir faire en d&#xe9;veloppement Mobile et RIA."/>
+</node>
+</node>
+<node COLOR="#111111" CREATED="1349111184634" ID="ID_93358017" MODIFIED="1349111936112" TEXT="COMPETENCES TECHNIQUES MISE EN PRATIQUE :">
+<node COLOR="#111111" CREATED="1349111196850" ID="ID_289684392" MODIFIED="1349111908863" TEXT="- Framework FLEX 4.5 (Spark, Flash AS3, MXML, BlazeDs, Swiz)"/>
+<node COLOR="#111111" CREATED="1349111223170" ID="ID_1093936166" MODIFIED="1349111908863" TEXT="- Framework Spring"/>
+<node COLOR="#111111" CREATED="1349111235402" ID="ID_742247097" MODIFIED="1349111908863" TEXT="- Test unitaire JUnit4"/>
+<node COLOR="#111111" CREATED="1349111250450" ID="ID_502082949" MODIFIED="1349111908863" TEXT="- D&#xe9;ploiement multi-plateformes IPad / Android / AIR"/>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1349111123170" ID="ID_1207663804" MODIFIED="1349111935051" TEXT="">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1349111283215" ID="ID_846996311" MODIFIED="1349111936114" TEXT="F&#xe9;vrier 2012 - Juin 2012">
+<node COLOR="#111111" CREATED="1349111294434" ID="ID_1169768866" MODIFIED="1349111936598" TEXT="MISSION : Dans le cadre d&apos;une prestation en r&#xe9;gie pour le client decathlon, j&apos;ai particip&#xe9; au projet de refonte totale de la page produit de leur site e-commerce. Je suis intervenu &#xe0; la fois sur le Back-Office et sur le Front-Office sur les cycles de d&#xe9;veloppement et de recette du projet.">
+<node COLOR="#111111" CREATED="1349111340410" ID="ID_455892315" MODIFIED="1349111908868" TEXT="Projet Osmose - E-motion"/>
+<node COLOR="#111111" CREATED="1349111367242" ID="ID_548179494" MODIFIED="1349111908868" TEXT="http://www.decathlon.fr/ "/>
+</node>
+<node COLOR="#111111" CREATED="1349111428338" ID="ID_1301029104" MODIFIED="1349111936601" TEXT="">
+<node COLOR="#111111" CREATED="1349111436655" ID="ID_368298564" MODIFIED="1349111936606" TEXT="Comp&#xe9;tences mises en pratique :">
+<node COLOR="#111111" CREATED="1349111441626" ID="ID_1822755303" MODIFIED="1349111908869" TEXT="- JEE "/>
+<node COLOR="#111111" CREATED="1349111451858" ID="ID_329466712" MODIFIED="1349111908869" TEXT="- Framework Apache OFBIZ"/>
+<node COLOR="#111111" CREATED="1349111466650" ID="ID_1079438991" MODIFIED="1349111908869" TEXT="- HTML5 / CSS3 / jQuery"/>
+<node COLOR="#111111" CREATED="1349111475634" ID="ID_1098403101" MODIFIED="1349111908869" TEXT="- R&#xe9;daction de sp&#xe9;cification techniques."/>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#990000" CREATED="1349111505058" ID="ID_456163192" MODIFIED="1349111935053" TEXT="Juillet 2012 - Septembre 2012">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1349111524794" ID="ID_135284697" MODIFIED="1349111936115" TEXT="MISSION : Dans le cadre d&apos;une prestation en r&#xe9;gie pour le client decathlon, j&apos;ai particip&#xe9; au projet d&apos;externalisation du processus de paiement de leur site e-commerce.">
+<node COLOR="#111111" CREATED="1349111599906" ID="ID_455869681" MODIFIED="1349111908872" TEXT="Projet Osmose Easy-Pay"/>
+</node>
+<node COLOR="#111111" CREATED="1349111621802" ID="ID_1264967283" MODIFIED="1349111936117" TEXT="">
+<node COLOR="#111111" CREATED="1349111629678" ID="ID_771387792" MODIFIED="1349111936603" TEXT="Comp&#xe9;tences mises en pratique :">
+<node COLOR="#111111" CREATED="1349111632394" ID="ID_774492988" MODIFIED="1349111908872" TEXT="- JEE / OFBIZ"/>
+<node COLOR="#111111" CREATED="1349111650874" ID="ID_1452276954" MODIFIED="1349111908873" TEXT="- HTML5 / JQuery"/>
+<node COLOR="#111111" CREATED="1349111667058" ID="ID_400396734" MODIFIED="1349111908873" TEXT="- Groovy"/>
+<node COLOR="#111111" CREATED="1349111678794" ID="ID_630925533" MODIFIED="1349111908873" TEXT="- WebServices / WSDL / SOAPUI"/>
+<node COLOR="#111111" CREATED="1349111758994" ID="ID_804878567" MODIFIED="1349111908873" TEXT="- R&#xe9;daction de documentation."/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1349111792450" ID="ID_472708563" MODIFIED="1349111925730" POSITION="left" TEXT="Centres d&apos;int&#xe9;r&#xea;t">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1349111832674" ID="ID_827218782" MODIFIED="1349111908874" TEXT="Sports (Football, Arts martiaux, Natation) ">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1349111859986" ID="ID_563348235" MODIFIED="1349111908875" TEXT="Lecture">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1349111874242" ID="ID_880538698" MODIFIED="1349111908875" TEXT="Musique">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1349111878810" ID="ID_513454735" MODIFIED="1349111908875" TEXT="Voyages">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1349111889642" ID="ID_587999087" MODIFIED="1349111908875" TEXT="Nouvelles technologies.">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
